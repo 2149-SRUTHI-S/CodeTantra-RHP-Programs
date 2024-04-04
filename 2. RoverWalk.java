a35e930b299dd12685bdd72ff5cd34ade97bf7f0
Sruthi Sivasankararaj <129876043+2149-SRUTHI-S@users.noreply.github.com>
2024-04-04 21:14:03 +0530

    Update to 2. RoverWalk.java

--- 2. RoverWalk.java
+++ 2. RoverWalk.java
@@ -61,7 +61,7 @@ public class RoverWalk
         for(i = 0; i < m; i++)
         dp[i][0] = 1;
         for(i = 0; i < n; i++)
-        dp[0][j] = 1;
+        dp[0][i] = 1;
         for(i = 1; i < m; i++){
             for(j = 1; j < n; j++){
                 dp[i][j] = dp[i-1][j] + dp[i][j-1];
