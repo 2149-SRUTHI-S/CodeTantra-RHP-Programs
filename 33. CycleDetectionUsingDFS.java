4d8908d22e3231966f4493a43fc1bed5b8b6de14
Sruthi Sivasankararaj <129876043+2149-SRUTHI-S@users.noreply.github.com>
2024-06-12 10:26:02 +0530

    Update 33. CycleDetectionUsingDFS.java

--- 33. CycleDetectionUsingDFS.java
+++ 33. CycleDetectionUsingDFS.java
@@ -75,3 +75,93 @@ class Main { // Graph
         sc.close();
     }
 }
+
+// Hamiltonian Cycle detection in directed graphs using DFS--> Recursion Stack(Back edges)
+
+import java.util.*;
+
+class Main { // Graph
+    int V;
+    int path[];
+    Main(int v) {
+        V = v;
+    }
+
+    void hamiltonianCycle(int AM[][]) {
+        path = new int[V];
+        for (int i = 0; i < V; i++) {
+            path[i] = -1;
+        }
+        
+        path[0] = 0;
+        if (Helper(AM, path, 1) == false) {
+            System.out.print("No solution");
+            return;
+        }
+        
+        display(path);
+        return;
+    }
+
+    void display(int path[]) {
+        for (int i = 0; i < V; i++) {
+            System.out.print(path[i] + "-->");
+        }
+        System.out.print(path[0]);
+
+    }
+    boolean Helper(int AM[][], int path[], int p) {
+        
+        // base condition if all vertices are included
+        if (p == V) {
+            // if there is an edge from the last included
+            if (AM[path[p - 1]][path[0]] == 1) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+        
+        for (int i = 1; i < V; i++) {
+            if (state(i, AM, path, p) == true) {
+                path[p] = i;
+                if (Helper(AM, path, p + 1) == true) {
+                    return true;
+                }
+                path[p] = -1;
+
+            }
+        }
+        return false;
+    }
+
+
+    boolean state(int i, int AM[][], int path[], int p) {
+        if (AM[path[p - 1]][i] == 0) {
+            return false;
+
+        }
+        for (int j = 0; j < V; j++) {
+            if (path[j] == i) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+
+    public static void main(String arg[]) {
+        Scanner sc = new Scanner(System.in);
+        int v = sc.nextInt();
+        Main graph = new Main(v);
+        int AM[][] = new int[v][v];
+        for (int i = 0; i < v; i++) {
+            for (int j = 0; j < v; j++) {
+                AM[i][j] = sc.nextInt();
+            }
+        }
+        
+        graph.hamiltonianCycle(AM);
+        sc.close();
+    }
+}
