98cc9fcdac1f4da5a17b2b5683cef81ff2ed6213
Sruthi Sivasankararaj <129876043+2149-SRUTHI-S@users.noreply.github.com>
2024-07-04 21:11:05 +0530

    Update 41. SubsetSumDP.java

--- 41. SubsetSumDP.java
+++ 41. SubsetSumDP.java
@@ -46,30 +46,3 @@ class Main {
             System.out.print("No");
     }
 }
-
-// Bottum up DP
-
-import java.util.*;
-
-class Main {
-
-    public static int Catalan(int n) {
-        int table[] = new int[n + 1]; // to store the result of sub problem
-        table[0] = 1;
-        table[1] = 1;
-        
-        for (int i = 2; i <= n; i++) { // n
-            for (int j = 0; j < i; j++) {
-                table[i] = table[i] + (table[j] * table[i - j - 1]); // 2
-            }
-        }
-        
-        return table[n];
-    }
-    
-    public static void main(String args[]) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        System.out.print(Catalan(n));
-    }
-}
